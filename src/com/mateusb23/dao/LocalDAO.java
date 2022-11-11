fcb1a9c916687d4543dbc8e66cf03302820f0aff
MATEUS SANTOS BISPO <mateusb.bispo@gmail.com>
2022-11-10 22:56:00 -0300

    Updating the LocalDAO() class

--- src/com/mateusb23/dao/LocalDAO.java
+++ src/com/mateusb23/dao/LocalDAO.java
@@ -6,13 +6,13 @@ import com.mateusb23.connection.ConnectionFactory;
 import com.mateusb23.modelo.Local;
 
 public class LocalDAO {
-	
+
 	public Local findById(Long id) {
-		
+
 		EntityManager em = new ConnectionFactory().getEntityManager();
-		
+
 		Local local = null;
-		
+
 		try {
 			local = em.find(Local.class, id);
 		} catch (Exception e) {
@@ -20,49 +20,53 @@ public class LocalDAO {
 		} finally {
 			em.close();
 		}
-		
+
 		return local;
 	}
 
-	public Local save(Local local) {
-		
+	public void update(Long id, Local local) {
+
 		EntityManager em = new ConnectionFactory().getEntityManager();
-		
+
 		try {
-			em.getTransaction().begin();
-			em.persist(local);
-			em.getTransaction().commit();
+			if (findById(id) != null) {
+				em.getTransaction().begin();
+				em.merge(local);
+				em.getTransaction().commit();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			em.getTransaction().rollback();
 		} finally {
 			em.close();
 		}
-		
-		return local;
+
 	}
-	
-	public Local update(Local local) {
-		
+
+	public void save(Long id, Local local) {
+
 		EntityManager em = new ConnectionFactory().getEntityManager();
-		
+
 		try {
-			em.getTransaction().begin();
-			em.merge(local);
-			em.getTransaction().commit();
+			if (findById(id) == null) {
+				em.getTransaction().begin();
+				em.persist(local);
+				em.getTransaction().commit();
+			} else {
+				em.getTransaction().rollback();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			em.close();
 		}
-		
-		return local;
+
 	}
-	
+
 	public void removeById(Long id) {
-		
+
 		EntityManager em = new ConnectionFactory().getEntityManager();
-		
+
 		try {
 			em.getTransaction().begin();
 			Local local = findById(id);
@@ -76,9 +80,9 @@ public class LocalDAO {
 		} finally {
 			em.close();
 		}
-		
+
 	}
-	
-	
-	
+
+
+
 }
